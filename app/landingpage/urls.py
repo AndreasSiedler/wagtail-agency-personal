cb616170e482161c4a38237dfecd56e197a57878
Christina Neumayer <christinaneumayer@Christinas-MacBook-Air.local>
2020-06-18 12:33:52 +0200

    added debug-toolbar-code but not working

--- app/landingpage/urls.py
+++ app/landingpage/urls.py
@@ -1,6 +1,7 @@
 from django.conf import settings
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.urls import path
 
 from wagtail.admin import urls as wagtailadmin_urls
 from wagtail.core import urls as wagtail_urls
@@ -18,14 +19,21 @@ urlpatterns = [
 
 ]
 
-
+# If you are in DEV Mode
 if settings.DEBUG:
+    # Django Debug Toolbar
+    import debug_toolbar
+    urlpatterns = [
+        path('__debug__/', include(debug_toolbar.urls)),
+    ] + urlpatterns
+
     from django.conf.urls.static import static
     from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
     # Serve static and media files from development server
     urlpatterns += staticfiles_urlpatterns()
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
 
 urlpatterns = urlpatterns + [
     # For anything not caught by a more specific rule above, hand over to
