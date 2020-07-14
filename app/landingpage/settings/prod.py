16da218e142e8e1db0c779d98db5f9db2e1919fd
Christina Neumayer <christinaneumayer@christinas-air.home>
2020-07-14 18:26:11 +0200

    improvement: production static url update

--- app/landingpage/settings/prod.py
+++ app/landingpage/settings/prod.py
@@ -2,9 +2,6 @@ from .base import *
 
 DEBUG = False
 
-STATIC_ROOT = '/static_files'
-STATIC_URL = '/static/'
-
 SECRET_KEY = 'kjfasjijlkj242345löjk2h4jh32jh542jk2lj4hx'
 
 ALLOWED_HOSTS = ['siedler.co', 'www.siedler.co', '64.227.113.104']
