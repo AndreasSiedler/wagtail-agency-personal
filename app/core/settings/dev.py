d5b38b0292f63b1c6f8cd4aa465c5e07d1ac90ed
Andreas Siedler <andreas.siedler@gmail.com>
2020-07-22 14:44:28 +0200

    debug 404 page and sending emails via smtp

--- app/core/settings/dev.py
+++ app/core/settings/dev.py
@@ -4,20 +4,15 @@ from .base import *
 DEBUG = True
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '2yhgz74eb6eff5zxin1ntn-q^b+(gv8a#$p06j66_qig(gb7%c'
+SECRET_KEY = 't-zl$)!^dhh$kiz8-)!rr02t8uhn4%v6nl*f*y62p9xmxy94dh'
 
 # SECURITY WARNING: define the correct hosts in production!
 ALLOWED_HOSTS = ['*'] 
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
-# Add Debug Configuration
-INSTALLED_APPS = INSTALLED_APPS + [
-    'debug_toolbar'
-]
 
-MIDDLEWARE = MIDDLEWARE + [
-    'debug_toolbar.middleware.DebugToolbarMiddleware',
-]
-
-INTERNAL_IPS = ('127.0.0.1', 'localhost', '0.0.0.0')
+try:
+    from .local import *
+except ImportError:
+    pass
--- app/core/urls.py
+++ app/core/urls.py
@@ -1,7 +1,6 @@
 from django.conf import settings
 from django.conf.urls import include, url
 from django.contrib import admin
-from django.urls import path
 
 from wagtail.admin import urls as wagtailadmin_urls
 from wagtail.core import urls as wagtail_urls
@@ -9,7 +8,6 @@ from wagtail.documents import urls as wagtaildocs_urls
 
 from search import views as search_views
 from wagtail.contrib.sitemaps.views import sitemap
-
 from core.views import robots_txt
 
 
@@ -23,21 +21,14 @@ urlpatterns = [
 
 ]
 
-# If you are in DEV Mode
-if settings.DEBUG:
-    # Django Debug Toolbar
-    import debug_toolbar
-    urlpatterns = [
-        path('__debug__/', include(debug_toolbar.urls)),
-    ] + urlpatterns
 
+if settings.DEBUG:
     from django.conf.urls.static import static
     from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
     # Serve static and media files from development server
     urlpatterns += staticfiles_urlpatterns()
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
 
 urlpatterns = urlpatterns + [
     # For anything not caught by a more specific rule above, hand over to
