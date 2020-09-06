120a599687936b233a0da678446af04ae4de2ef3
Andreas Siedler <andreas.siedler@gmail.com>
2020-09-06 21:22:19 +0200

    Offline compression activated

--- docker/prod/python/entrypoint.sh
+++ docker/prod/python/entrypoint.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-python manage.py collectstatic --noinput
 python manage.py migrate
+python manage.py collectstatic --noinput
+python manage.py compress
 
 echo "Running command '$*'"
 exec /bin/bash -c "$*"
